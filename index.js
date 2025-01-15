8252cd033e8163b1ec249b7a5e9499fa30faa894
rohanrta <rohanabraham26@gmail.com>
2025-01-15 19:42:06 +0530

    small changes are made

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+
 const jsonServer = require("json-server")
 const MPServer = jsonServer.create()
 
 const middleware=jsonServer.defaults()
 const PORT = 3000
-
+// 
 const route = jsonServer.router('db.json')
 MPServer.use(middleware)
 MPServer.use(route)
